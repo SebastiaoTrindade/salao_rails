c079d36664e263ff8b1d4090cf2b56e004e5cada
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-04-29 14:23:41 -0300

    Ajustando os controllers das seções

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Login realizado com sucesso!"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "Email ou senha inválidos."
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logout realizado com sucesso!"
+    redirect_to login_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,48 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  before_action :require_user, only: [:edit, :update]
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Cadastro realizado com sucesso!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      flash[:success] = "Dados atualizados com sucesso!"
+      redirect_to root_path
+    else
+      render 'edit' 
+    end
+  end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
+
+def set_user
+  @user = User.find(params[:id])
+end
+
+def require_user
+  unless current_user
+    flash[:danger] = "Você precisa estar logado para acessar essa página."
+    redirect_to login_path
   end
 end
