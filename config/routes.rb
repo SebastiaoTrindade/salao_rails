8e0a02a58a6c543fc6ad3724279e9e38206040b1
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-04-29 13:52:34 -0300

    Criando a seção login e usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # Static Pages  
   root 'pages#home'
+
+  # Sessions
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  # Users
+  resources :users, except: [:index] 
   
 end
