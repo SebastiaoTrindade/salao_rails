db670173ea8424b99dc88908feb4f5452ad4ccb0
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-05-30 22:56:03 -0300

    Finalizando a página de serviços

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_admin 
-    rediirect_to root_path unless current_user&.admin?
+    redirect_to root_path unless current_user&.admin?
   end
 
   def require_customer
