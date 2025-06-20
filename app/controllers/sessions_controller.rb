2eb32b8019622ac94d6379bc400d987c288f91b5
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-06-20 11:28:45 -0300

    Refatorando a página Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params[:email]&.downcase)
+
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Login realizado com sucesso!"
-      redirect_to root_path
+      redirect_to after_login_path(user)
     else
       flash.now[:danger] = "Email ou senha inválidos."
-      render 'new'
+      render :new
     end
   end
 
   def destroy
-    session[:user_id] = nil
+    reset_session
     flash[:success] = "Logout realizado com sucesso!"
     redirect_to root_path
+  end
+
+  private
+  def after_login_path(user)
+    user.admin? ? admin_dashboard_path : customers_dashboard_path 
   end
 end
